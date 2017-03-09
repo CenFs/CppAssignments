4d1fc3281a4c98e3639867872448f86087866908
Jingyi Bai <baij@linux-desktop7.cc.tut.fi>
2017-03-09 15:53:25 +0200

    update

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
-    return 0;
+vector<string> split(string str, char delimiter, bool ignore_empty) {
+  vector<string> internal;
+  stringstream ss(str);
+  string tok;
+
+  while(getline(ss, tok, delimiter)) {
+    internal.push_back(tok);
+  }
+  if (ignore_empty) {
+      for (int i = 0; i < internal.size(); i++) {
+          if (internal[i] == "") {
+              // cout << i << endl;
+              internal.erase(internal.begin()+i);
+          }
+      }
+  }
+
+  return internal;
+}
+
+int main(int argc, char **argv) {
+  string myCSV = "one,two,,three,four";
+  vector<string> sep = split(myCSV, ',', true);
+
+  // If using C++11 (which I recommend)
+  /* for(string t : sep)
+   *  cout << t << endl;
+   */
+
+  for(int i = 0; i < sep.size(); ++i)
+    cout << sep[i] << endl;
+  return 0;
 }
